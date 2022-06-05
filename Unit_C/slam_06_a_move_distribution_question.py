8929f6db05db5a6fe4e177d2554663b05b954948
Satish Bethi <satish@boominteractive.io>
2022-06-04 20:05:21 -0600

    - Modified distribution.py and part a to work with python3 - Finished Unit C Part a

--- Unit_C/slam_06_a_move_distribution_question.py
+++ Unit_C/slam_06_a_move_distribution_question.py
@@ -1,8 +1,8 @@
 # Move a distribution (in x) by a given amount (an integer).
 # 06_a_move_distribution
 # Claus Brenner, 26 NOV 2012
-from pylab import plot, show, ylim
 from distribution import *
+import matplotlib.pyplot as plt
 
 def move(distribution, delta):
     """Returns a Distribution that has been moved (x-axis) by the amount of
@@ -10,7 +10,7 @@ def move(distribution, delta):
 
     # --->>> Insert your code here.
     
-    return distribution  # Replace this by your own result.
+    return Distribution(distribution.offset + delta, distribution.values)  # Replace this by your own result.
 
 if __name__ == '__main__':
     # List of movements: move 3 times by 20.
@@ -18,13 +18,13 @@ if __name__ == '__main__':
 
     # Start with a known position: probability 1.0 at position 10.
     position = Distribution.triangle(10,2)
-    plot(position.plotlists(0,100)[0], position.plotlists(0,100)[1],
-         linestyle='steps')
+    plt.plot(position.plotlists(0,100)[0], position.plotlists(0,100)[1],
+         drawstyle='steps')
 
     # Now move and plot.
     for m in moves:
         position = move(position, m)
-        plot(position.plotlists(0,100)[0], position.plotlists(0,100)[1],
-             linestyle='steps')
-    ylim(0.0, 1.1)
-    show()
+        plt.plot(position.plotlists(0,100)[0], position.plotlists(0,100)[1],
+             drawstyle='steps')
+    plt.ylim(0.0, 1.1)
+    plt.show()
